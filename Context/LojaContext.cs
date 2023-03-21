ce3ad28e9bb34fa46e551dafb9fdb28de76c25b7
Glysonn <kauaglyson@gmail.com>
2023-03-21 18:30:27 -0300

    Settando as novas classes como tabelas

--- Context/LojaContext.cs
+++ Context/LojaContext.cs
@@ -16,11 +16,22 @@ namespace AttAnalise.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // HERDAM DE USUARIO
             modelBuilder.Entity<Cliente>()
                 .HasKey(u => u.Id);
 
             modelBuilder.Entity<Administrador>()
                 .HasKey(u => u.Id);
+            
+            // HERDAM DE PRODUTO
+            modelBuilder.Entity<Peca>()
+                .HasKey(u => u.Codigo);
+
+            modelBuilder.Entity<Periferico>()
+                .HasKey(u => u.Codigo);
+
+            modelBuilder.Entity<Acessorio>()
+                .HasKey(u => u.Codigo);
         }
     }
 }
