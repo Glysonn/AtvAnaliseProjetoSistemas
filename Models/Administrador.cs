ab39833f7707670ae194effc204b3f89a9ba930a
Glysonn <kauaglyson@gmail.com>
2023-03-21 20:48:06 -0300

    Passando algumas propriedades com valor padrão, dependendo da classe

--- Models/Administrador.cs
+++ Models/Administrador.cs
@@ -4,10 +4,12 @@ namespace AttAnalise.Models
 {
     public class Administrador : Usuario
     {
+        
 
-        public Administrador(int id, string nome, string email, string senha, TipoUsuario tipoUsuario) : base (id, nome, email, senha, tipoUsuario)
+        public Administrador(string nome, string email, string senha) : base (0, nome, email, senha, TipoUsuario.Administrador)
         {
-
+            // o valor de TipoUsuario é com valor padrão: Administrador (é passado para a classe pai (Usuario) através do construtor)
+            // o valor de ID é definido passado como 0 pois é AutoIncrement, então não é preciso especificar
         }
     }
 }
--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,9 +5,10 @@ namespace AttAnalise.Models
     public class Cliente : Usuario
     {
 
-        public Cliente(int id, string nome, string email, string senha, TipoUsuario tipoUsuario) : base (id, nome, email, senha, tipoUsuario)
+        public Cliente(int id, string nome, string email, string senha) : base (0, nome, email, senha, TipoUsuario.Cliente)
         {
-
+            // o valor de TipoUsuario é com valor padrão: Cliente (é passado para a classe pai (Usuario) através do construtor)
+            // o valor de ID é definido passado como 0 pois é AutoIncrement, então não é preciso especificar
         }
     }
 }
