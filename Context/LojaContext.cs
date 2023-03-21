9854e57b56d44dff4467568b7536beac7f89d024
Glysonn <kauaglyson@gmail.com>
2023-03-21 16:43:16 -0300

    Configurando o Banco de Dados

---
+++ Context/LojaContext.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using AttAnalise.Models;
+
+namespace AttAnalise.Context
+{
+    public class LojaContext : DbContext
+    {
+        public LojaContext(DbContextOptions<LojaContext> options) : base (options)
+        {
+            //
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Administrador> Administradores { get; set;}
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>()
+                .HasKey(u => u.Id);
+
+            modelBuilder.Entity<Administrador>()
+                .HasKey(u => u.Id);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using AttAnalise.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// adicionando o banco em memória do entity framework na API
+builder.Services.AddDbContext<LojaContext> (options => options.UseInMemoryDatabase("Loja"));
 
 var app = builder.Build();
 
