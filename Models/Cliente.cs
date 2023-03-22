80a90660ccd60a897b5fadba513a12c52b2bbf0c
Glysonn <kauaglyson@gmail.com>
2023-03-22 20:54:40 -0300

    Mudando os parâmetros do construtor
    Tirando o parâmetro ID, pois ele é passado para a classe base através da classe filha, é um valor que não precisa ser recebido no construtor.

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -5,7 +5,8 @@ namespace AttAnalise.Models
     public class Cliente : Usuario
     {
 
-        public Cliente(int id, string nome, string email, string senha) : base (0, nome, email, senha, TipoUsuario.Cliente)
+
+        public Cliente(string nome, string email, string senha) : base (0, nome, email, senha, TipoUsuario.Cliente)
         {
             // o valor de TipoUsuario é com valor padrão: Cliente (é passado para a classe pai (Usuario) através do construtor)
             // o valor de ID é definido passado como 0 pois é AutoIncrement, então não é preciso especificar
