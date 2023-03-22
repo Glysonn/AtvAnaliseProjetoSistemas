79b6c55935fa505c57f82a5c1a20e2a51b895711
Glysonn <kauaglyson@gmail.com>
2023-03-21 22:30:06 -0300

    (Administrador) GET: Criando os métodos GET
    O primeiro retorna todos os administradores e o segundo retorna UM administrador pelo ID

--- Controllers/AdministradorController.cs
+++ Controllers/AdministradorController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 
 using AttAnalise.Context;
+using AttAnalise.Models;
 
 namespace AttAnalise.Controllers
 {
@@ -13,6 +14,54 @@ namespace AttAnalise.Controllers
         {
             _context = context;
         }
-    }
 
+        // MÉTODOS HTTP GET
+        [HttpGet]
+        public IActionResult GetAdministradores()
+        {
+            try
+            {
+                // verifica se há algo de errado com a conexão do banco de dados
+                if (!_context.Database.CanConnect())
+                    return Problem( detail: "Erro: O banco de dados não foi encontrado!",
+                                    statusCode: StatusCodes.Status400BadRequest,
+                                    title: "Bad Request");
+                
+                var Administradores = _context.Administradores.ToList();
+
+                if (!Administradores.Any())
+                    return NotFound("Não há nenhum dado de administradores no sistema!");
+
+                return Ok(Administradores);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new {Error = ex.Message, Mensagem = "Aconteceu um erro interno no servidor!"});
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAdministradoresById(int id)
+        {
+            try
+            {
+                // verifica se há algo de errado com a conexão do banco de dados
+                if (!_context.Database.CanConnect())
+                    return Problem( detail: "Houve um problema com a conexão ao banco de dados",
+                                    statusCode: StatusCodes.Status400BadRequest,
+                                    title: "Bad Request");
+                
+                var AdministradorBanco = _context.Administradores.SingleOrDefault(a => a.Id == id);
+
+                if (AdministradorBanco == null)
+                    return NotFound($"Administrador de ID {id} não se encontra no sistema!");
+
+                return Ok(AdministradorBanco);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new {Error = ex.Message, Mensagem = "Aconteceu um erro interno no servidor!"});
+            }
+        }
+    }
 }
