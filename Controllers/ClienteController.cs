707288967c041cd9e502fb396bd6cde6ec46b367
Glysonn <kauaglyson@gmail.com>
2023-03-22 21:34:40 -0300

    (Cliente) GET: Criando os métodos GET

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -27,5 +27,53 @@ namespace AttAnalise.Controllers
             return null;
         }
 
+
+        // MÉTODOS HTTP GET
+        [HttpGet]
+        public IActionResult GetClientes()
+        {
+            try
+            {
+                // verifica se há algo de errado com a conexão do banco de dados
+                var responseBanco = ChecarConexaoBanco();
+                if (responseBanco != null)
+                    return responseBanco;
+                
+                var Clientes = _context.Clientes.ToList();
+
+                if (!Clientes.Any())
+                    return NotFound("Não há nenhum dado de clientes no sistema!");
+
+                return Ok(Clientes);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new {Error = "Aconteceu um erro interno no servidor!", Mensagem = ex.Message});
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetClientesById(int id)
+        {
+            try
+            {
+                // verifica se há algo de errado com a conexão do banco de dados
+                var responseBanco = ChecarConexaoBanco();
+                if (responseBanco != null)
+                    return responseBanco;
+                
+                var ClienteBanco = _context.Clientes.SingleOrDefault(a => a.Id == id);
+
+                if (ClienteBanco == null)
+                    return NotFound($"O Cliente de ID {id} não se encontra no sistema!");
+
+                return Ok(ClienteBanco);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new {Error = "Aconteceu um erro interno no servidor!", Mensagem = ex.Message});
+            }
+        }
+
     }
 }
