0dc6c937e26a63b2402c5f4a43389396375c691e
Glysonn <kauaglyson@gmail.com>
2023-03-25 20:41:55 -0300

    Criando Classes Request
    Classes Request serão apenas para receber os dados na requisição. É necessária pois as validações ocorrem na classe padrão, porém na requisição não é necessário fazer as validações ao receber os dados (as validações são feitas APÓS receber os dados).

---
+++ Models/Requests/UsuarioRequest.cs
@@ -0,0 +1,36 @@
+using System.Security.Cryptography;
+using System.Text;
+
+using AttAnalise.Enums;
+
+namespace AttAnalise.Models.Requests
+{
+    // !!! CLASSES REQUEST SÃO SOMENTE PARA FAZER A REQUISIÇÃO PARA O USUÁRIO. !!!
+    // Pois a validação não precisa ser feita na hora que o dado for enviado, mas sim na hora que for submetido
+    // EXEMPLO: No método HTTP PUT, o valor pode ser enviado como vazio, para permanecer o mesmo valor (isso é tratado na controller)
+
+    public class UsuarioRequest
+    {
+        public int Id { get => _Id; set => _Id = value; }
+        public string Nome
+        {   get => _Nome;
+            set => _Nome = value;
+        }
+        public string Email
+        {   get => _Email;
+            set => _Email = value;
+        }
+        public string Senha
+        {   get => _Senha;
+            set => _Senha = value;
+        }
+        public TipoUsuario TipoUsuario { get => _TipoUsuario; set => _TipoUsuario = value; }
+        
+        
+        private int _Id { get; set; }
+        private string _Nome { get; set; }
+        private string _Email { get; set; }
+        private string _Senha { get; set; }
+        private TipoUsuario _TipoUsuario { get; set; }
+    }
+}
