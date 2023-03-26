95485247cde32d899e16eadddab70c42faa1747e
Glysonn <kauaglyson@gmail.com>
2023-03-26 03:15:12 -0300

    (Peca) GET: Retornar todas as pecas cadastradas

--- Models/Peca.cs
+++ Models/Peca.cs
@@ -2,7 +2,17 @@ namespace AttAnalise.Models
 {
     public class Peca : Produto
     {
-        public string Arquitetura { get => _Arquitetura; set => _Arquitetura = value; }
+        public string Arquitetura
+        {
+            get => _Arquitetura;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new ArgumentException("A arquitetura não pode ser um campo vazio! Por favor, preencha o campo corretamente");
+                else
+                    _Arquitetura = value;
+            }
+        }
         
         private string _Arquitetura { get; set; }
         public Peca(string nome, string tipo, string marca, string modelo, decimal valor, string arquitetura) : base (0, nome, tipo, marca, modelo, valor)
